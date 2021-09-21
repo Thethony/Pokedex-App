cbc2d51ebb50ddbd2490daf504f322aff855e373
Thethony <anthonyyimlee@gmail.com>
2021-09-21 14:49:50 -0400

    Testing api form script/ jquery elements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$function(){
+$(function() {
     //constant variables
-const Base_url="https://pokeapi.co/api/v2/pokemon/"
+const pokemon_url="https://pokeapi.co/api/v2/pokemon/"
+let pokemonData;
+
+// elements
+// const $type
+// const $height
+// const $health
+// const 
+$.ajax(`https://pokeapi.co/api/v2/pokemon/`).then(function(data){
+    const pokemonData = data;
+    console.log(pokemonData)
+})
 
 
-}
+
+});

bcda25944c9eb3420958a97dacaa5927e494821c
Thethony <anthonyyimlee@gmail.com>
2021-09-21 17:17:06 -0400

    added all script to get data from api

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
 $(function() {
     //constant variables
-const pokemon_url="https://pokeapi.co/api/v2/pokemon/"
-let pokemonData;
+    const pokemon_url="https://pokeapi.co/api/v2/pokemon/"
+    let pokemonData;
+    // elements
+    const $type = $("#type");
+    const $height = $("#height");
+    const $health = $("#health");
+    const $weight = $("#weight");
+    const $form = $('form');
+    const $input = $('input[type="text"]');
+    const $photo = $("image")
+    $form.on('submit', handleGetData)
+    
+    function handleGetData(event) {
+            event.preventDefault();
+            const pokemonName = $input.val();
+            $input.val("");
+            $.ajax(`${pokemon_url}${pokemonName}`).then(function(data){
+                pokemonData = data;
+                console.log(pokemonData);
+                render();
+            }, function(error) {
+                console.log(error);
+            });
 
-// elements
-// const $type
-// const $height
-// const $health
-// const 
-$.ajax(`https://pokeapi.co/api/v2/pokemon/`).then(function(data){
-    const pokemonData = data;
-    console.log(pokemonData)
-})
+    };
 
-
+    function render(){
+        $type.text("Type: " + pokemonData.types[0].type.name)
+        $height.text("Height: " + pokemonData.height)
+        $health.text("Health: " + pokemonData.stats[0].base_stat)
+        $weight.text("weight: " + pokemonData.weight)
+    }
 
 });
